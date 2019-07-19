2391f4db34e6e9364f4625674acd0efddfa52a8c
Daniel Pauli <dapaulid@gmail.com>
2019-07-19 18:15:27 +0200

    Add error registry.

---
+++ lib/L1_transport/errors.ts
@@ -0,0 +1,2 @@
+export const GENERIC       = { label: 'L1_GENERIC',       code: 0xE1000000, text: 'Unknown transport layer error' };
+export const ENCODE_FAILED = { label: 'L1_ENCODE_FAILED', code: 0xE1000001, text: 'Encoding a message failed' };
---
+++ lib/errors.ts
@@ -0,0 +1,33 @@
+import * as L1 from './L1_transport/errors';
+
+interface IErrorDesc {
+    label: string;
+    code: number;
+    text: string;
+}
+
+// error class used for all library specific errors
+class RemoError extends Error implements IErrorDesc {
+
+    constructor(err: IErrorDesc) {
+        // format error message
+        const message = '[' + err.label + '] ' + err.text + ' (code ' + formatErrorCode(err.code) + ')';
+        // call base
+        super(message);
+        // set properties
+        this.name = "RemoError";
+        this.label = err.label;
+        this.code = err.code;
+        this.text = err.text;
+    }
+
+    public label: string;
+    public code: number;
+    public text: string;
+}
+
+function formatErrorCode(code: number) {
+    return '0x' + ("00000000" + code.toString(16).toUpperCase()).substr(-8);
+}
+
+export { RemoError, L1 };
