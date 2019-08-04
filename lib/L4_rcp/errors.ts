c2ced73bea983c9667516ffdb02756b301d86781
Daniel Pauli <dapaulid@gmail.com>
2019-08-04 19:16:02 +0200

    Fix error code range of rpc errors.

--- lib/L4_rcp/errors.ts
+++ lib/L4_rcp/errors.ts
@@ -8,9 +8,9 @@
  */
 //------------------------------------------------------------------------------
 
-export const GENERIC               = { label: 'L4_GENERIC',               code: 0xE2000000, text: 'Unknown application layer error' };
-export const FUNC_NOT_FOUND        = { label: 'L4_FUNC_NOT_FOUND',        code: 0xE2000001, text: 'Function not found' };
-export const UNKNOWN_RPC_ERROR     = { label: 'L4_UNKNOWN_RPC_ERROR',     code: 0xE2000002, text: 'Unknown RPC error, see details' };
+export const GENERIC               = { label: 'L4_GENERIC',               code: 0xE4000000, text: 'Unknown RPC layer error' };
+export const FUNC_NOT_FOUND        = { label: 'L4_FUNC_NOT_FOUND',        code: 0xE4000001, text: 'Function not found' };
+export const UNKNOWN_RPC_ERROR     = { label: 'L4_UNKNOWN_RPC_ERROR',     code: 0xE4000002, text: 'Unknown RPC error, see details' };
 
 //------------------------------------------------------------------------------
 // end of file
